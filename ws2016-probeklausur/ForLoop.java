4d0f880467cf0a08c32bfd88084442160fa18c65
B Kleinen <kleinen@infrastructure.de>
2017-01-26 08:02:39 +0100

    for loop assignment

---
+++ ws2016-probeklausur/ForLoop.java
@@ -0,0 +1,22 @@
+
+/**
+ * This is a
+ * 
+ * @author Barne Kleinen 
+ */
+public class ForLoop
+{
+    /**
+     * Your assignment is to implement this method:
+     * Sum up every element in the array passed as
+     * an argument and return the sum
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sum(int[] a)
+    {
+        // replace this with your solution
+        return -1;     
+    }
+}
